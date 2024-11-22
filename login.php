f4c1e18af7da05d24051e04f947c5b976d16cbc6
arnaudglorian <arnaud.glorian@isen.yncrea.fr>
2024-11-22 10:33:36 +0100

    modif DB

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ $postData = $_POST;
 
 try {
     // On se connecte à MySQL
-    $mysqlClient = new PDO('mysql:host=localhost;dbname=webdev;charset=utf8', 'root', '',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],);
+    $mysqlClient = new PDO('mysql:host=localhost;dbname=`zoo`;charset=utf8', 'root', '',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],);
 } catch (Exception $e) {
     // En cas d'erreur, on affiche un message et on arrête tout
     die('Erreur : ' . $e->getMessage());
