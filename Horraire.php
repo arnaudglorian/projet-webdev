fda211d51fb343b68fbbb4830d3a12f5d295a592
arnaudglorian <arnaud.glorian@isen.yncrea.fr>
2024-12-13 11:51:50 +0100

    horraire admin

---
+++ Horraire.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horraire</title>
+    <!-- Lien vers le fichier CSS -->
+    <link rel="stylesheet" href="avis3.css">
+</head>
+<body>
+
+<header class="header">
+    <h1>Changer les horraire d'un enclo</h1>
+</header>
+
+<nav>
+    <a href="accueil.php">Accueil</a>
+    <a href="#contact">Contact</a>
+    <a href="billeterie.html">Billeterie</a>
+    <a href="Nosservices.html">Nos services</a>
+    <a href="lesanimaux3.php">Nos enclos et animaux</a>
+    
+</nav>
+
+<?php require(__DIR__ . '/updatemeal.php')?>
+
+<footer id="contact">
+    <p>Contactez-nous : parcanimalier@labarbene.fr | Téléphone : 01 23 45 67 89</p>
+</footer>
+
+</body>
+</html>
---
+++ updatemeal.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$postData = $_POST;
+
+try {
+    // On se connecte à MySQL
+    $mysqlClient = new PDO('mysql:host=localhost;dbname=zoo;charset=utf8', 'root', '',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],);
+} catch (Exception $e) {
+    // En cas d'erreur, on affiche un message et on arrête tout
+    die('Erreur : ' . $e->getMessage());
+}
+?>
+<?php if (isset($postData['enclo']) &&  isset($postData['repas'])) :?>
+	<?php $sqlQuery = 'UPDATE enclosures SET meal=:repas WHERE `id`=:enclo ';
+	$resultStatement = $mysqlClient->prepare($sqlQuery);
+	$resultStatement->execute(['enclo' => $postData['enclo'],'repas'=>$postData['repas']]);?>
+    <div> L'heure a bien été mise à jour</div>
+<?php endif;?>
+
+    <form action="horraire.php" id="loginForm" method="POST">
+    	<div class="mb-3">
+			<div id="email-help" class="form-text">Entrez l'id de l'enclo qui doit changer d'heure de</div>
+        	<input class="form-control" id="enclo" name="enclo" placeholder="id de l'enclo" required>
+    	</div>
+    	<div class="mb-3">
+        	<label class="form-label">Entrez la nouvelle heure de repas</label>
+        	<input class="form-control" id="repas" name="repas" required>
+    	</div>
+    	<button type="submit" class="login-btn">changer l'heure</button>
+	</form>
