8ff5a5b8d5fdf507ae920e70e9d81ed0cfa2306f
Arthur-bot1234 <jourdier.arthur@gmail.com>
2024-12-19 14:17:31 +0100

    up

--- Horraire.php
+++ Horraire.php
@@ -3,19 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Horraire</title>
+    <title>Horaire</title>
     <!-- Lien vers le fichier CSS -->
-    <link rel="stylesheet" href="avis3.css">
+    <link rel="stylesheet" href="horaire.css">
 </head>
 <body>
 
 <header class="header">
-    <h1>Changer les horraire d'un enclo</h1>
+    <h1>Changement Horaire Enclos</h1>
 </header>
 
 <nav>
     <a href="accueil.php">Accueil</a>
-    <a href="#contact">Contact</a>
     <a href="billeterie.html">Billeterie</a>
     <a href="Nosservices.html">Nos services</a>
     <a href="lesanimaux3.php">Nos enclos et animaux</a>
@@ -24,9 +23,7 @@
 
 <?php require(__DIR__ . '/updatemeal.php')?>
 
-<footer id="contact">
-    <p>Contactez-nous : parcanimalier@labarbene.fr | Téléphone : 01 23 45 67 89</p>
-</footer>
+
 
 </body>
 </html>
